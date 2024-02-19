4e62acbefd0e2763fd6909973aeae932b5d02fe5
vilsafur <vilsafur@gmail.com>
2024-02-19 22:15:04 +0100

    fix: actorData

--- module/data/actorData.mjs
+++ module/data/actorData.mjs
@@ -1,11 +1,11 @@
 export class ActorData extends foundry.abstract.TypeDataModel {
   static defineSchema() {
-    const Fields = foundry.data.Fields
+    const Fields = foundry.data.fields
 
     console.debug(`Brigandyne | ActorData`)
 
     return {
-      nom: Fields.StringField({
+      nom: new Fields.StringField({
         required: true,
         label: "Brigandyne.ActorData.Nom"
       })
