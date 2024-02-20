88ae650827d83bc8c4d531191e77b0ba11b0fe70
cyril_p <cyril.poidevin@ageo.fr>
2024-02-20 16:26:44 +0100

    refacto(CommonActorData): skillField

--- module/data/CommonActorData.mjs
+++ module/data/CommonActorData.mjs
@@ -1,165 +1,37 @@
 const fields = foundry.data.fields;
 
+const skillField = () => ({
+  battle: new fields.SchemaField({
+    base: new fields.NumberField({
+      initial: 0,
+      min: 0
+    }),
+    progress: new fields.NumberField({
+      initial: 0,
+      min: 0,
+      max: 0,
+      step: 5
+    })
+  })
+})
+
 export class CommonActorData extends foundry.abstract.TypeDataModel {
   static defineSchema() {
     return {
       skills: new fields.SchemaField({
-        combat: new fields.SchemaField({
-          base: new fields.NumberField({
-            initial: 0,
-            min: 0
-          }),
-          progression: new fields.NumberField({
-            initial: 0,
-            min: 0,
-            max: 0,
-            step: 5
-          })
-        }),
-        connaissances: new fields.SchemaField({
-          base: new fields.NumberField({
-            initial: 0,
-            min: 0
-          }),
-          progression: new fields.NumberField({
-            initial: 0,
-            min: 0,
-            max: 0,
-            step: 5
-          })
-        }),
-        discretion: new fields.SchemaField({
-          base: new fields.NumberField({
-            initial: 0,
-            min: 0
-          }),
-          progression: new fields.NumberField({
-            initial: 0,
-            min: 0,
-            max: 0,
-            step: 5
-          })
-        }),
-        endurance: new fields.SchemaField({
-          base: new fields.NumberField({
-            initial: 0,
-            min: 0
-          }),
-          progression: new fields.NumberField({
-            initial: 0,
-            min: 0,
-            max: 0,
-            step: 5
-          })
-        }),
-        force: new fields.SchemaField({
-          base: new fields.NumberField({
-            initial: 0,
-            min: 0
-          }),
-          progression: new fields.NumberField({
-            initial: 0,
-            min: 0,
-            max: 0,
-            step: 5
-          })
-        }),
-        habilete: new fields.SchemaField({
-          base: new fields.NumberField({
-            initial: 0,
-            min: 0
-          }),
-          progression: new fields.NumberField({
-            initial: 0,
-            min: 0,
-            max: 0,
-            step: 5
-          })
-        }),
-        magie: new fields.SchemaField({
-          base: new fields.NumberField({
-            initial: 0,
-            min: 0
-          }),
-          progression: new fields.NumberField({
-            initial: 0,
-            min: 0,
-            max: 0,
-            step: 5
-          })
-        }),
-        mouvement: new fields.SchemaField({
-          base: new fields.NumberField({
-            initial: 0,
-            min: 0
-          }),
-          progression: new fields.NumberField({
-            initial: 0,
-            min: 0,
-            max: 0,
-            step: 5
-          })
-        }),
-        perception: new fields.SchemaField({
-          base: new fields.NumberField({
-            initial: 0,
-            min: 0
-          }),
-          progression: new fields.NumberField({
-            initial: 0,
-            min: 0,
-            max: 0,
-            step: 5
-          })
-        }),
-        sociabilite: new fields.SchemaField({
-          base: new fields.NumberField({
-            initial: 0,
-            min: 0
-          }),
-          progression: new fields.NumberField({
-            initial: 0,
-            min: 0,
-            max: 0,
-            step: 5
-          })
-        }),
-        survie: new fields.SchemaField({
-          base: new fields.NumberField({
-            initial: 0,
-            min: 0
-          }),
-          progression: new fields.NumberField({
-            initial: 0,
-            min: 0,
-            max: 0,
-            step: 5
-          })
-        }),
-        tir: new fields.SchemaField({
-          base: new fields.NumberField({
-            initial: 0,
-            min: 0
-          }),
-          progression: new fields.NumberField({
-            initial: 0,
-            min: 0,
-            max: 0,
-            step: 5
-          })
-        }),
-        volonte: new fields.SchemaField({
-          base: new fields.NumberField({
-            initial: 0,
-            min: 0
-          }),
-          progression: new fields.NumberField({
-            initial: 0,
-            min: 0,
-            max: 0,
-            step: 5
-          })
-        })
+        battle: new fields.SchemaField(skillField()),
+        knowledge: new fields.SchemaField(skillField()),
+        discretion: new fields.SchemaField(skillField()),
+        endurance: new fields.SchemaField(skillField()),
+        strength: new fields.SchemaField(skillField()),
+        ability: new fields.SchemaField(skillField()),
+        magic: new fields.SchemaField(skillField()),
+        movement: new fields.SchemaField(skillField()),
+        perception: new fields.SchemaField(skillField()),
+        sociability: new fields.SchemaField(skillField()),
+        survival: new fields.SchemaField(skillField()),
+        shooting: new fields.SchemaField(skillField()),
+        willingness: new fields.SchemaField(skillField())
       }),
       description: new fields.HtmlField({
         initial: ""
